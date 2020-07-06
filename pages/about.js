52cdf113f38c0453e494144b0d22b711d8646425
Mahmudul Alam <mahmud@pop-os.localdomain>
2020-07-06 13:48:46 +0600

    About page added

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,8 @@
 import Head from "next/head";
 import Layout from "../components/Layout";
+import AboutMe from "../components/About/AboutMe";
+import Services from "../components/About/Services";
+import Testimonial from "../components/About/Testimonial";
 
 const About = () => {
   return (
@@ -8,7 +11,26 @@ const About = () => {
         <Head>
           <title>Mahmudul Alam || About</title>
         </Head>
-        <h1>Hello from abouts</h1>
+        <div className="mi-about-area mi-section mi-padding-top">
+          <div className="container">
+            <AboutMe />
+          </div>
+        </div>
+        <div className="mi-service-area mi-section mi-padding-top">
+          <div className="container">
+            <Services />
+          </div>
+        </div>
+
+        <div className="mi-review-area mi-section mi-padding-top mi-padding-bottom">
+          <div className="container">
+            <div className="mi-sectiontitle">
+              <h2>Reviews</h2>
+              <span>Reviews</span>
+            </div>
+            <Testimonial />
+          </div>
+        </div>
       </div>
     </Layout>
   );
