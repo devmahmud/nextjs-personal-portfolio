51682a913fafbb7e11d6c5c252dfb2f79a999fac
Mahmudul Alam <expelmahmud@gmail.com>
2020-07-06 19:16:05 +0600

    add experience data

---
+++ pages/api/data.js
@@ -0,0 +1,45 @@
+export const work_experiences = [
+  {
+    title: "Software Engineer",
+    company: "Heavytask LLC",
+    from: "2020",
+    to: "Present",
+    details:
+      "Working as a full time software engineer. Python( Django) and React Developer",
+  },
+  {
+    title: "Python Developer",
+    company: "Software Grid",
+    from: "2019",
+    to: "2020",
+    details:
+      "Fulltime python developer. Machine Learning and Natural Language Processing Project.",
+  },
+];
+
+export const educations = [
+  {
+    title: "Bsc in CSE",
+    institute: "Daffodil International University",
+    from: "2015",
+    to: "2019",
+    details:
+      "I have successfully completed Bachelor of Computer Science & Engineering From Daffodil International University with CGPA of 3.80 out of 4.00",
+  },
+  {
+    title: "Higher School Certificate",
+    institute: "New govt. degree college Rajshahi",
+    from: "2012",
+    to: "2014",
+    details:
+      "I have successfully completed Secondary School Certificate with a GPA 4.70 out of 5 From Science Group.",
+  },
+  {
+    title: "Secondary School Certificate",
+    institute: "Naogaon K.D Govt High School",
+    from: "2010",
+    to: "2007",
+    details:
+      "I have studied from class Six To Class Eight In Agradigun M.L High school , then i moved to Naogaon K.D govt high school and Completed my Secondary School Certificate With Golden GPA 5.",
+  },
+];
